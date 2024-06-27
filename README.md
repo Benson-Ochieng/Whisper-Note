483434656f3f1a5e93662da35c853687dfd8b79e
Benson (Mark) Ochieng <markbenson10368@gmail.com>
2024-06-27 16:38:13 +0300

    Secrete message and how to encode and decode the characters

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Whisper-Note
+
+An app that can share secrete messages.
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-alert("Hello");
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+    document.querySelector('#message').classList.add('hide');
+    document.querySelector('#message-shown').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    document.querySelector('#message').classList.add('hide');
+    document.querySelector('#link').classList.remove('hide');
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
+});
